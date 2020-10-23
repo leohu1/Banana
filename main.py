3d75c0b60b30d2ff4be6c2b6bd0a61a7ab90f6af
leohu1 <47905372+leohu1@users.noreply.github.com>
2020-10-23 23:04:16 +0800

    banana
    v1.1

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+#!/Users/huqiwei/anaconda3/bin/python3
 from Banana_core import Banana
+from fire import Fire
 
-Banana(
-"""set a 1
-print (add 10 a)"""
-).run()
+# Banana(
+# """set a 1
+# print (add 10 a)"""
+# ).run()
+
+
+def main(file):
+    """
+    Banana language
+    :param file: file name
+    :return:
+    """
+    Data = open(file).read()
+    Banana(Data).run()
+
+
+if __name__ == '__main__':
+    Fire(main)
